e6a50de8fa7b92ef79e452f3d5df1153b62db69b
Khalifa Umar <umarkhalifa138@gmail.com>
2022-11-12 07:30:23 +0100

    updated country card

--- lib/Presentation/screens/home_view/widgets/country_detail.dart
+++ lib/Presentation/screens/home_view/widgets/country_detail.dart
@@ -1,3 +1,4 @@
+import 'package:all_countries/Presentation/screens/detail_view/detail_view_portrait.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
@@ -15,62 +16,68 @@ class CountryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: SizedBox(
-            height: 50,
-            width: 50,
-            child: Image.network(
-              country.flag!,
-              fit: BoxFit.cover,
-              loadingBuilder: (context, child, loadingProgress) {
-                if (loadingProgress == null) {
-                  return child;
-                }
-                return  Center(
-                  child: SpinKitChasingDots(
-                    color: theme.primaryColor,
-                  ),
-                );
-              },
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailPortrait(country: country),
+          ),
+        );
+      },
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: SizedBox(
+              height: 50,
+              width: 50,
+              child: Image.network(
+                country.flag!,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  }
+                  return Center(
+                    child: SpinKitChasingDots(
+                      color: theme.primaryColor,
+                    ),
+                  );
+                },
 
-              //! IMAGE FETCH ERROR
-              errorBuilder: (context, object, trace) {
-                return  Center(
-                  child: Icon(
-                    Icons.error,
-                    color: theme.primaryColor,
-                    size: 20,
-                  ),
-                );
-              },
+                //! IMAGE FETCH ERROR
+                errorBuilder: (context, object, trace) {
+                  return Center(
+                    child: Icon(
+                      Icons.error,
+                      color: theme.primaryColor,
+                      size: 20,
+                    ),
+                  );
+                },
+              ),
             ),
           ),
-        ),
-        const SizedBox(
-          width: 20,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              country.name!,
-              style: TextStyle(
-                  color: theme.colorScheme.onBackground),
-            ),
-            Text(
-              country.capital == null
-                  ? "NA"
-                  : country.capital![0],
-              style: TextStyle(
-                  color: theme.colorScheme.secondary,
-                  fontSize: 11),
-            ),
-          ],
-        ),
-      ],
+          const SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                country.name!,
+                style: TextStyle(color: theme.colorScheme.onBackground),
+              ),
+              Text(
+                country.capital == null ? "NA" : country.capital![0],
+                style:
+                    TextStyle(color: theme.colorScheme.secondary, fontSize: 11),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
